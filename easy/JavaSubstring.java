64593ee5ce839f07068a7d44a1a8a08614bad723
Pablo Zalem <zalem.pablo@yahoo.com>
2026-06-03 08:51:41 -0300

    Substrings

---
+++ easy/JavaSubstring.java
@@ -0,0 +1,16 @@
+package easy;
+
+import java.util.Scanner;
+
+public class JavaSubstring {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        String S = scan.nextLine();
+        int start = scan.nextInt();
+        int end = scan.nextInt();
+
+        System.out.println(S.substring(start, end));
+
+        scan.close();
+    }
+}
